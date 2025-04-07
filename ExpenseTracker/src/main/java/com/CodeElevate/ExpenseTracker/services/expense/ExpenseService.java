2d46f8a8e8ff3cc5e6e9d4c161dbeda6699ccb79
Santhoji07 <santhoji07msd@gmail.com>
2025-04-08 02:49:10 +0530

    created project,connected to mysql database,created expense entity,dto,repository,service,Cretaed POST,GET expense API calls

---
+++ ExpenseTracker/src/main/java/com/CodeElevate/ExpenseTracker/services/expense/ExpenseService.java
@@ -0,0 +1,15 @@
+package com.CodeElevate.ExpenseTracker.services.expense;
+
+import com.CodeElevate.ExpenseTracker.dto.ExpenseDTO;
+import com.CodeElevate.ExpenseTracker.entity.Expense;
+
+import java.util.List;
+
+public interface ExpenseService {
+
+    Expense postExpense(ExpenseDTO expenseDTO);
+
+    List<Expense> getAllExpenses();
+
+    Expense getExpenseById(Long id);
+}
