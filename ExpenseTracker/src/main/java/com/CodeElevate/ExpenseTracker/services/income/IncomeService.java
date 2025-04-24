17ecadebeaee856710437f8c013e3a4368bdf84a
Sameeksha-nayak15 <sameeksha1511nayak@gmail.com>
2025-04-24 15:21:23 +0530

    part 2 sameeksha work fixed

--- ExpenseTracker/src/main/java/com/CodeElevate/ExpenseTracker/services/income/IncomeService.java
+++ ExpenseTracker/src/main/java/com/CodeElevate/ExpenseTracker/services/income/IncomeService.java
@@ -1,4 +1,16 @@
 package com.CodeElevate.ExpenseTracker.services.income;
 
+import com.CodeElevate.ExpenseTracker.dto.IncomeDTO;
+import com.CodeElevate.ExpenseTracker.entity.Income;
+
+import java.util.List;
+
 public interface IncomeService {
+    Income postIncome(IncomeDTO incomeDTO);
+
+    List<IncomeDTO> getAllIncomes();
+
+    Income updateIncome(Long id, IncomeDTO incomeDTO);
+
+
 }
