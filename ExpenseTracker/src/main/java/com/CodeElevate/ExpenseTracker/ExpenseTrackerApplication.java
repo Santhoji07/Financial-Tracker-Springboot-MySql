84707318f0e4c3abf6f7d2eb665bad043982533f
Santhoji07 <santhoji07msd@gmail.com>
2025-05-09 17:57:10 +0530

    Deplyoment

--- ExpenseTracker/src/main/java/com/CodeElevate/ExpenseTracker/ExpenseTrackerApplication.java
+++ ExpenseTracker/src/main/java/com/CodeElevate/ExpenseTracker/ExpenseTrackerApplication.java
@@ -2,6 +2,9 @@ package com.CodeElevate.ExpenseTracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ExpenseTrackerApplication {
@@ -10,4 +13,16 @@ public class ExpenseTrackerApplication {
 		SpringApplication.run(ExpenseTrackerApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("https://ledgemate.netlify.app")  // Replace with your Netlify frontend URL
+						.allowedMethods("*")
+						.allowedHeaders("*");
+			}
+		};
+	}
 }
